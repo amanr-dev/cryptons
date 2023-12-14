7be60a9f636ee9ecf7c85d7fb65de9f705aa9ece
Aman <dv451197@gmail.com>
2023-12-14 23:22:31 +0530

    fixed cryptoDetails

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -12,52 +12,108 @@ const CryptoDetails = () => {
   const dispatch = useDispatch();
   const status = useSelector((store) => store.coin.status);
   const data = useSelector((store) => store.coin.data);
+  const cryptoDetails = data?.data?.coin;
   const [timePeriod, setTimePeriod] = useState("24h");
 
-  if (status === "loading") {
-    return (
-      <Box
-        display="flex"
-        width="100%"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Bars color="#001529" width={50} height={50} visible={true} />
-      </Box>
-    );
-  }
+  console.log({ data, status });
 
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
 
-  // const stats = [
-  //   { title: 'Price to USD', value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`, icon: <DollarCircleOutlined /> },
-  //   { title: 'Rank', value: cryptoDetails?.rank, icon: <NumberOutlined /> },
-  //   { title: '24h Volume', value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`, icon: <ThunderboltOutlined /> },
-  //   { title: 'Market Cap', value: `$ ${cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)}`, icon: <DollarCircleOutlined /> },
-  //   { title: 'All-time-high(daily avg.)', value: `$ ${cryptoDetails?.allTimeHigh?.price && millify(cryptoDetails?.allTimeHigh?.price)}`, icon: <TrophyOutlined /> },
-  // ];
+  const stats = [
+    {
+      title: "Price to USD",
+      value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`,
+      icon: <DollarCircleOutlined />,
+    },
+    { title: "Rank", value: cryptoDetails?.rank, icon: <NumberOutlined /> },
+    {
+      title: "24h Volume",
+      value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`,
+      icon: <ThunderboltOutlined />,
+    },
+    {
+      title: "Market Cap",
+      value: `$ ${
+        cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)
+      }`,
+      icon: <DollarCircleOutlined />,
+    },
+    {
+      title: "All-time-high(daily avg.)",
+      value: `$ ${
+        cryptoDetails?.allTimeHigh?.price &&
+        millify(cryptoDetails?.allTimeHigh?.price)
+      }`,
+      icon: <TrophyOutlined />,
+    },
+  ];
 
-  // const genericStats = [
-  //   { title: 'Number Of Markets', value: cryptoDetails?.numberOfMarkets, icon: <FundOutlined /> },
-  //   { title: 'Number Of Exchanges', value: cryptoDetails?.numberOfExchanges, icon: <MoneyCollectOutlined /> },
-  //   { title: 'Aprroved Supply', value: cryptoDetails?.supply?.confirmed ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
-  //   { title: 'Total Supply', value: `$ ${cryptoDetails?.supply?.total && millify(cryptoDetails?.supply?.total)}`, icon: <ExclamationCircleOutlined /> },
-  //   { title: 'Circulating Supply', value: `$ ${cryptoDetails?.supply?.circulating && millify(cryptoDetails?.supply?.circulating)}`, icon: <ExclamationCircleOutlined /> },
-  // ];
+  const genericStats = [
+    {
+      title: "Number Of Markets",
+      value: cryptoDetails?.numberOfMarkets,
+      icon: <FundOutlined />,
+    },
+    {
+      title: "Number Of Exchanges",
+      value: cryptoDetails?.numberOfExchanges,
+      icon: <MoneyCollectOutlined />,
+    },
+    {
+      title: "Aprroved Supply",
+      value: cryptoDetails?.supply?.confirmed ? (
+        <CheckOutlined />
+      ) : (
+        <StopOutlined />
+      ),
+      icon: <ExclamationCircleOutlined />,
+    },
+    {
+      title: "Total Supply",
+      value: `$ ${
+        cryptoDetails?.supply?.total && millify(cryptoDetails?.supply?.total)
+      }`,
+      icon: <ExclamationCircleOutlined />,
+    },
+    {
+      title: "Circulating Supply",
+      value: `$ ${
+        cryptoDetails?.supply?.circulating &&
+        millify(cryptoDetails?.supply?.circulating)
+      }`,
+      icon: <ExclamationCircleOutlined />,
+    },
+  ];
 
   // example response
   // https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd=24h
-  // useEffect(() => {
-  //   if (status === "idle") {
-  //     dispatch(fetchCoinData(`${coinUrl}${coinId}?timePeriod=${timePeriod}`));
-  //   }
 
-  //   console.log({ data, status });
-  // }, [coinId]);
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchCoinData(`${coinUrl}${coinId}?timePeriod=${timePeriod}`));
+    } else if (status === "loading") {
+      return (
+        <Box
+          display="flex"
+          width="100%"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Bars color="#001529" width={50} height={50} visible={true} />
+        </Box>
+      );
+    }
+
+    // console.log({ data, status });
+  }, [coinId]);
 
   // https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd?timePeriod=24h
 
-  return <Box component="section">{coinId}</Box>;
+  return (
+    <Box component="section">
+      <Typography variant="subtitle2">Hello to crypto details</Typography>
+    </Box>
+  );
 };
 
 export default CryptoDetails;
